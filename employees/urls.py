4397c0403f40ee673fe8b6c8b6a2cdd3e46dcabd
Bright Kanyange <brightkanyange@outlook.com>
2019-06-12 09:58:04 +0300

    Working Navigation

--- employees/urls.py
+++ employees/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.dashboard_page, name="dashboard_page"),
+    path('employees/', views.employees_page, name="employees_page"),
+    path('payroll/', views.payroll_page, name="payroll_page"),
 ]
--- employees/views.py
+++ employees/views.py
@@ -3,5 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 # dashboard
-def index(request):
-    return render(request,'employees/dashboard.html') 
+def dashboard_page(request):
+    context = {
+        "dashboard": "active"
+    }
+    return render(request,'employees/dashboard.html', context) 
+
+def employees_page(request):
+    context = {
+        "employees": "active"
+    }
+    return render(request,'employees/employees.html', context)
+
+def payroll_page(request):
+    context = {
+        "payroll": "active"
+    }
+    return render(request,'employees/payroll.html', context)
