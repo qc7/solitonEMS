6072fd3b52333b19a90e93f1cc5cd6708156d67c
Bright Kanyange <brightkanyange@outlook.com>
2019-06-14 17:54:14 +0300

    Added the Employee model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+    start_date    = models.DateField()
+    marital_status = models.CharField(max_length=10)
+    dob     = models.DateField()
+    nationality = models.CharField(max_length=20)
+    nssf_no = models.CharField(max_length=20)
+    telephone_no = models.CharField(max_length=20)
+    residence_address = models.CharField(max_length=20)
+    national_id   = models.CharField(max_length=20)
+    ura_tin = models.CharField(max_length=20)
+    image_url = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    
+    
