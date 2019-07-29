7ee2c2ddbeac28e106d8af16eee97f0c1a08e48e
Bright Kanyange <brightkanyange@outlook.com>
2019-07-29 19:07:30 +0300

    Worked on notifications

--- employees/procedures.py
+++ employees/procedures.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from role.models import Notification
 def redirect_user_role(request):
     user = request.user
     # If user is an employee
@@ -7,3 +8,9 @@ def redirect_user_role(request):
     # If user is HOD
     if str(user.solitonuser.soliton_role) == 'HOD':
         return render(request,"role/hod.html")
+
+# Send notification
+def send_notification(solitonuser,message):
+    notification = Notification(user=solitonuser,message=message)
+    notification.save()
+
