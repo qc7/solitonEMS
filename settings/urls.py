866339fe666539a8b5ba64582c7486dfacb9b5b9
Bright Kanyange <brightkanyange@outlook.com>
2019-08-15 11:40:33 +0300

    Created settings app

---
+++ settings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+urlpatterns = [
+    # Pages
+    # path('', views.leave_dashboard_page, name="leave_dashboard_page")
+
+]
