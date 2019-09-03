809dabb904bf964f87f09dc06c469bf7e5eba3fb
Walusimbi Hakim <hakim.walusimbi@soliton.co.ug>
2019-09-03 10:00:58 +0300

    Working on leave Planner

--- leave/views.py
+++ leave/views.py
@@ -4,10 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.utils.dateparse import parse_date
 from django.utils import timezone
 import datetime
-from django.db.models import Sum
+from django.db.models import (
+    Sum,
+    Count
+)
 from datetime import timedelta
 import calendar
-#from .procedures import get_leave_balance, get_employee_leave
+from collections import namedtuple
 from employees.models import Employee
 from .models import (
     Leave_Types, 
@@ -132,7 +135,7 @@ def edit_leave_type_page(request, id):
 
     context = {
         "leave_page": "active",
-        "leave": leave
+        "leave": leave  
     }
     return render(request, 'leave/leave_type.html', context)
 
@@ -235,8 +238,9 @@ def apply_leave(request):
         s_date = request.POST["s_date"]
         e_date = request.POST["e_date"]
 
-        n_days = calculateWorkingDaysBetweenTwoDates(s_date, e_date)
-        l_days =  l_type.leave_days #getting the leave type entitlement        
+        #getting the leave type entitlement
+        n_days = calculate_leave_days(s_date, e_date)
+        l_days =  l_type.leave_days         
         
         if n_days <= l_days:
             new_balance=0
@@ -273,7 +277,7 @@ def apply_leave(request):
                     return render(request,"role/employee/leave.html")      
 
         else:
-            messages.warning(request, f'You cannot Request for more than the\
+            messages.warning(request, f'You cannot Request({n_days}) for more than the\
                 {l_type.leave_type} leave days ({l_type.leave_days})')
             return render(request,"role/employee/leave.html")
 
@@ -315,29 +319,30 @@ def approve_leave(request):
             messages.success(request, 'Leave Approved Successfully')
             return redirect('leave_dashboard_page') 
             
-def calculateWorkingDaysBetweenTwoDates(start_date, end_date):
-    holidays = Holidays.objects.all()
-
+def calculate_leave_days(start_date, end_date):    
     date_format = "%Y-%m-%d"
+    from_date = datetime.datetime.strptime(start_date, date_format)
+    to_date = datetime.datetime.strptime(end_date, date_format)
     
-    date_difference = datetime.datetime.strptime(end_date, date_format)\
-             - datetime.datetime.strptime(start_date, date_format)  
+    date_difference = to_date - from_date  
 
     all_days_between = (date_difference.days + 1)
 
-    all_working_days = 0
+    holidays = Holidays.objects.filter(holiday_date__range=(from_date, to_date)).count()
+
+
+    all_working_days = all_days_between - holidays
     k=0
     while k <= all_days_between:
-        check_date = parse_date(start_date) + datetime.timedelta(days = k)
-
-        for holiday in holidays:           
-            if check_date.weekday() != 6:
-                if check_date != holiday.holiday_date: 
-                    all_working_days = all_working_days + 1
+        check_date = from_date + datetime.timedelta(days = k)
+        
+        if check_date.weekday() == 6:
+            all_working_days = all_working_days - 1
         
         k = k + 1
        
-    return all_working_days 
+    return all_working_days
+        
 
 def leave_calendar(request):
     # first_day = calendar.TextCalendar(calendar.MONDAY)
@@ -398,3 +403,25 @@ def add_new_absence(request):
         messages.error(request, f'Data Not Saved, Check you inputs and try again!')
 
         return redirect('leave_planner')
+    
+def get_leave_overlap(start_date, end_date):
+    absences = leave_planer.objects.all()
+
+    Range = namedtuple('Range', ['start', 'end'])
+
+    r1 = Range(start=start_date, end=end_date)
+    
+    overlap = 0
+    overlap_count = 0
+    for absence in absences:
+        r2 = Range(start=absence.start_date, end=absence.end_date)
+        latest_start = max(r1.start, r2.start)
+        earliest_end = min(r1.end, r2.end)
+        delta = (earliest_end - latest_start).days + 1
+        overlap = max(0, delta)
+
+        if overlap > 0:
+            overlap_count += 1
+
+    
+    return overlap_count
