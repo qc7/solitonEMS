45bceadf22c0d51a47317b06a36f17478a3920b1
Bright Kanyange <brightkanyange@outlook.com>
2020-01-07 17:25:05 +0300

    Done fixing holiday module

--- holidays/views.py
+++ holidays/views.py
@@ -1,17 +1,55 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
-@login_required
-def holidays_page(request):
-    # The line requires the user to be authenticated before accessing the view responses.
-    if not request.user.is_authenticated:
-        # if the user is not authenticated it renders a login page
-        return render(request, 'ems_auth/login.html', {"message": None})
+from django.urls import reverse
 
+from ems_auth.decorators import ems_login_required
+from holidays.models import Holiday
+from holidays.selectors import get_all_holidays, get_holiday
+from holidays.services import create_holiday
+
+
+@ems_login_required
+def holidays_page(request):
+    if request.POST:
+        date = request.POST.get('date')
+        name = request.POST.get('name')
+        holiday = create_holiday(date, name)
+        messages.success(request, "Successfully created the holiday")
+        return HttpResponseRedirect(reverse('holidays_page'))
+
+    holidays = get_all_holidays()
     context = {
         "holidays_page": "active",
+        "holidays": holidays
     }
 
     return render(request, 'holidays/holidays_page.html', context)
+
+
+def delete_holiday(request, holiday_id):
+    holiday = get_holiday(holiday_id)
+    holiday.delete()
+    messages.success(request, "Deleted a holiday entry")
+    return HttpResponseRedirect(reverse('holidays_page'))
+
+
+def edit_holiday_page(request, holiday_id):
+    holiday = get_holiday(holiday_id)
+    if request.POST:
+        date = request.POST.get('date')
+        name = request.POST.get('name')
+        Holiday.objects.filter(id=holiday.id).update(
+            name=name,
+            date=date
+        )
+        messages.success(request, "Updated the holiday entry")
+        return HttpResponseRedirect(reverse('holidays_page'))
+    context = {
+        "holidays_page": "active",
+        "holiday": holiday
+    }
+    return render(request, 'holidays/edit_holiday.html', context)
