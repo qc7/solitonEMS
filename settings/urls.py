2a4ec62508053f98f9ce96f06aa6a42b4dfd8a57
Bright Kanyange <brightkanyange@outlook.com>
2020-01-09 09:30:37 +0300

    updated settings

--- settings/urls.py
+++ settings/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     # Pages
-    path('', views.settings_page, name="settings_page")
-
+    path('', views.settings_page, name="settings_page"),
+    path('add_currency/', views.add_currency, name="add_currency"),
+    path('edit_currency/<int:currency_id>/', views.edit_currency_page, name="edit_currency_page"),
+    path('delete_currency/<int:currency_id>/', views.delete_currency, name="delete_currency"),
 ]
