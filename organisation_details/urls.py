4aff3bd44b3cb41ebc32edebcfae89280e098ba1
Bright Kanyange <brightkanyange@outlook.com>
2020-01-27 12:21:16 +0300

    Done creating a feature organisation details

--- organisation_details/urls.py
+++ organisation_details/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.about_us, name="about_us"),
+    path('no_organisation_detail_page/', views.no_organisation_detail_page, name="no_organisation_detail_page")
 ]
