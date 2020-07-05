5d772ee6b8c550c7bbe7e48588240e980470a1b0
Bright Kanyange <brightkanyange@outlook.com>
2020-07-05 20:37:08 +0300

    Separated pending overtime applications

--- overtime/selectors.py
+++ overtime/selectors.py
@@ -81,28 +81,40 @@ def get_hod_in_department(approver):
     pass
 
 
-def get_pending_overtime_applications(approver):
-    pending_applications = OvertimeApplication.objects.none()
-    is_supervisor = get_is_supervisor_in_team(approver)
-    is_hod = get_is_hod_in_department(approver)
+def get_pending_overtime_applications(approver_user: User) -> dict:
+    """Return a dictionary of pending applications per user role"""
+    hod_pending_applications = OvertimeApplication.objects.none()
+    hr_pending_applications = OvertimeApplication.objects.none()
+    cfo_pending_applications = OvertimeApplication.objects.none()
+    ceo_pending_applications = OvertimeApplication.objects.none()
+    supervisor_pending_applications = OvertimeApplication.objects.none()
+    is_supervisor = get_is_supervisor_in_team(approver_user)
+    is_hod = get_is_hod_in_department(approver_user)
 
     if is_hod:
-        hod_department = approver.solitonuser.employee.department
-        pending_applications = get_hod_pending_overtime_applications(hod_department)
+        hod_department = approver_user.solitonuser.employee.department
+        hod_pending_applications = get_hod_pending_overtime_applications(hod_department)
 
-    if approver.is_hr:
-        pending_applications = get_hr_pending_overtime_applications()
+    if approver_user.is_hr:
+        hr_pending_applications = get_hr_pending_overtime_applications()
 
-    if approver.is_cfo:
-        pending_applications = get_cfo_pending_overtime_applications()
+    if approver_user.is_cfo:
+        cfo_pending_applications = get_cfo_pending_overtime_applications()
 
-    if approver.is_ceo:
-        pending_applications = get_ceo_pending_overtime_applications()
+    if approver_user.is_ceo:
+        ceo_pending_applications = get_ceo_pending_overtime_applications()
 
     if is_supervisor:
-        supervisor = approver.solitonuser.employee
-        pending_applications = get_supervisor_pending_overtime_applications(supervisor)
+        supervisor = approver_user.solitonuser.employee
+        supervisor_pending_applications = get_supervisor_pending_overtime_applications(supervisor)
 
+    pending_applications = {
+        "supervisor_pending_applications": supervisor_pending_applications,
+        "hod_pending_applications": hod_pending_applications,
+        "hr_pending_applications": hr_pending_applications,
+        "cfo_pending_applications": cfo_pending_applications,
+        "ceo_pending_applications": ceo_pending_applications,
+    }
     return pending_applications
 
 
